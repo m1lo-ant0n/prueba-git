4a4584550481b03c5878c2b5fd436527d0be009f
m1lo-ant0n <milo.anton1993@gmail.com>
2023-07-21 19:00:04 -0300

    agrego archivos

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("holamundo\n");
+
+        System.out.printf("bye");
+
+        }
+    }
